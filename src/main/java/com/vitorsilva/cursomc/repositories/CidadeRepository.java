136bcd8c0fca4c7db0783c5327f39d081e85cf11
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2019-03-20 11:30:46 -0300

    Estado e Cidade

---
+++ src/main/java/com/vitorsilva/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.vitorsilva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vitorsilva.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
